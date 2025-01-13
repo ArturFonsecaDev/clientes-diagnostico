12d27257c6e4c2c0108d7dfd8e80fa0f20d9773a
trdntb <130498186+trdntb@users.noreply.github.com>
2025-01-13 12:09:00 -0300

    ApexCharts added

--- front-clientes/src/main.js
+++ front-clientes/src/main.js
@@ -6,9 +6,11 @@ import Quasar from "quasar/dist/quasar.umd.min.js";
 import "quasar/dist/quasar.min.css";
 import "@quasar/extras/material-icons/material-icons.css";
 import vueTheMask from "vue-the-mask";
+import VueApexCharts from "vue-apexcharts";
 
 Vue.use(Quasar);
 Vue.use(vueTheMask);
+Vue.use(VueApexCharts);
 
 Vue.config.productionTip = false;
 
